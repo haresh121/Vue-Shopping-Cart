cb17b870c2be10a8b31d5aa9cb5fc8f3e46d70e9
HARESH <34397532+haresh121@users.noreply.github.com>
2020-07-02 10:55:58 +0530

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,68 @@
+var inventory = [{
+  'name': 'USB Stick',
+
+  'options': [{
+      'id': 1,
+      'details': '16GB',
+      'price': 749
+  }, {
+      'id': 2,
+      'details': '32GB',
+      'price': 1299
+  }, {
+      'id': 3,
+      'details': '64GB',
+      'price': 1699
+  }]
+}, {
+  'name': 'USB Plug',
+
+  'options': [{
+      'id': 4,
+      'price': 499,
+      'details': '3ft cable'
+  }, {
+      'id': 13,
+      'price': 1099,
+      'details': '6ft cable'
+  }]
+}, {
+  'name': 'Small Phone',
+
+  'options': [{
+      'id': 5,
+      'price': 19999,
+      'details': 'Nokia Phone'
+  }]
+}, {
+  'name': 'Camera',
+
+  'options': [{
+      'id': 6,
+      'price': 7999,
+      'details': 'Blue Camera'
+  }, {
+      'id': 7,
+      'price': 8999,
+      'details': 'Red Camera'
+  }, {
+      'id': 8,
+      'price': 5599,
+      'details': 'Yellow Camera'
+  }, {
+      'id': 9,
+      'price': 45.50,
+      'details': 'Purple Camera'
+  }]
+}];
+var incart = [{
+  price: 10000,
+  listitem: 'First Test Product',
+  quantity: 4,
+  id: 1
+}, {
+  price: 1000,
+  listitem: 'Another Test Product',
+  quantity: 2,
+  id: 2
+}];
